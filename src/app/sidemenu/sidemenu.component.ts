57c2396ba459932269f35f81196b3e5d32cb43b0
ShivamShokeen007 <121612821+ShivamShokeen007@users.noreply.github.com>
2023-01-06 15:05:23 +0530

    sidemenu

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -5,10 +5,9 @@ import * as multi_side_menu_mock from '../mock/multi-level-left-menu.json';
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+  styleUrls: ['./sidemenu.component.css'],
 })
 export class SidemenuComponent implements OnInit, DoCheck {
-
   navitem?: any = (sidemenu_mock as any).default;
   navitemMulti?: any = (multi_side_menu_mock as any).default;
   // navitem = [
@@ -39,58 +38,116 @@ export class SidemenuComponent implements OnInit, DoCheck {
   //   },
   // ];
   navSelected = 1;
-  menuToggle : boolean = false;
+  menuToggle: boolean = false;
 
-  collapseMenu : any= [];
+  collapseMenu: any = [];
+  collapseMenu2: any = [];
+  collapseMenu3: any = [];
 
-  collapseMenu2 : any= [];
-
+  expanded :any;
+  selectedItems :any;
+  navSelectedID : any;
   constructor() {}
 
   ngOnInit(): void {
-    localStorage.setItem('menu-selected','Dashboard');
-    this.navitem = this.navitem.sort(
-      (p1: any, p2: any) =>
-        p2.sequence < p1.sequence ? 1 : p2.sequence > p1.sequence ? -1 : 0
+    localStorage.setItem('menu-selected', 'Dashboard');
+    this.navitem = this.navitem.sort((p1: any, p2: any) =>
+      p2.sequence < p1.sequence ? 1 : p2.sequence > p1.sequence ? -1 : 0
     );
 
-    this.navitemMulti = this.navitemMulti.sort(
-      (p1: any, p2: any) =>
-        p2.sequence < p1.sequence ? 1 : p2.sequence > p1.sequence ? -1 : 0
+    this.navitemMulti = this.navitemMulti.sort((p1: any, p2: any) =>
+      p2.sequence < p1.sequence ? 1 : p2.sequence > p1.sequence ? -1 : 0
     );
 
-    console.log("this.navitemMulti",this.navitemMulti);
+    console.log('this.navitemMulti', this.navitemMulti);
   }
 
-  menuCollapse(i:number){
-    if(this.collapseMenu[i]){
+  clearCollapse(){
+    this.collapseMenu = [];
+  }
+
+  menuCollapse(i: number) {
+    this.collapseMenu = [];
+    this.collapseMenu2 = [];
+    this.collapseMenu3 = [];
+    if (this.collapseMenu[i]) {
       this.collapseMenu[i] = false;
-    }
-    else{
+    } else {
       this.collapseMenu[i] = true;
     }
+
+
+
+    // console.log("this.collapseMenu",this.collapseMenu);
+    // console.log("this.collapseMenu 2",this.collapseMenu2);
+    // console.log("this.collapseMenu 3",this.collapseMenu3);
+    // this.collapseMenu2 = false;
+    // this.collapseMenu3 = false;
   }
 
-  menuCollapse2(i:number){
-    if(this.collapseMenu2[i]){
-      this.collapseMenu2[i] = false;
+  selectItem(item:any, id:any) {
+    this.selectedItems[id] = item;
+    console.log('rin')
+      }
+
+  isSelectedItem(item:any, id:any) {
+    console.log("this.selectedItems",this.selectedItems);
+    if(this.selectedItems?.[id]){
+      console.log('true');
+        return false
     }
     else{
+      console.log('true 1',this.selectedItems?.[id] && this.selectedItems?.[id] === item);
+
+
+      return this.selectedItems?.[id] && this.selectedItems?.[id] === item;
+
+
+    }
+    
+  };
+
+  onItemSelected(item: any) {
+    if (item.child_menu_items && item.child_menu_items.length) {
+      this.expanded[item.id] = !this.expanded[item.id];
+    }
+}
+
+  menuCollapse2(i: number) {
+    if (this.collapseMenu2[i]) {
+      this.collapseMenu2[i] = false;
+    } else {
       this.collapseMenu2[i] = true;
     }
   }
 
-  navSelect(i:number,name:string){
-    this.navSelected = i;
-    localStorage.setItem('menu-selected',name);
+  menuCollapse3(i: number) {
+    if (this.collapseMenu3[i]) {
+      this.collapseMenu3[i] = false;
+    } else {
+      this.collapseMenu3[i] = true;
+    }
   }
 
-  ngDoCheck(){
+  navSelect(i: number, item: any) {
+    // this.navSelected = item.id;
+    this.navSelected = i;
+    localStorage.setItem('menu-selected', item);
+  }
+
+  recurr(i:any){
+    console.log('recurr',i);
+  }
+
+  getj(i:any){
+    console.log("j",i);
+  }
+
+  ngDoCheck() {
     let checkToggle = localStorage.getItem('menu_toggle');
-    if(checkToggle == 'true'){
+    if (checkToggle == 'true') {
       this.menuToggle = true;
-    }
-    else{
+    } else {
       this.menuToggle = false;
     }
   }
